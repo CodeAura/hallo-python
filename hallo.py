19e8577e6f5cd9053344ffc7efeff970ec383ff3
ValidCoder <mikedh.prive@gmail.com>
2021-09-07 10:05:56 +0200

    Hier is mijn eerste code

---
+++ hallo.py
@@ -0,0 +1,15 @@
+# Mike
+# 99067408
+
+# Hello wereld van…                                    
+
+    # pppp    y     y   ttttttt   h   h    oooo   nnnn          
+    # p   p    y   y       t      h   h   o    o  n   n
+    # p   p     y y        t      h   h   o    o  n   n
+    # pppp       y         t      hhhhh   o    o  n   n
+    # p          y         t      h   h   o    o  n   n
+    # p          y         t      h   h   o    o  n   n
+    # p          y         t      h   h    oooo   n   n
+
+
+print("Hallo wereld van Python") # Hier laat hij een text zien in de console
